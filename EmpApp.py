d55b82b4cd328945259ec2fc27c41394db5a1a76
GohSJ <73984059+GohSJ@users.noreply.github.com>
2022-04-10 12:13:08 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -34,7 +34,7 @@ def about():
 
 @app.route("/GetEmp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template('ListEmp.html', getData=GetEmp)
+    return render_template('ListEmp.html', GetEmp=GetEmp)
 
 
 @app.route("/addemp", methods=['POST'])
